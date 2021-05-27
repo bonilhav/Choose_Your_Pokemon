44f6a84dadd310486d7cb35543661a692be7d248
Landon Waddell <Landon.waddell@gmail.com>
2021-05-26 20:27:48 -0600

    Added fetch to the Giphy APi

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function pokemon() {
     })
 }
 pokemon();
+
+
+const $miamiF = document.getElementById('miami');
+fetch('https://api.giphy.com/v1/gifs/search?api_key=MFLGZadukzit9Mk8qCC8J3cbVDWy11db&q=dogs=pt')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function ({ data }) {
+    console.log(data);
+    });
