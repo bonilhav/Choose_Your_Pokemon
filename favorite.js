2ac693dc8a39a4f5cb2bcb29f30ff8a9b2c2afa3
Landon Waddell <Landon.waddell@gmail.com>
2021-06-05 13:54:05 -0600

    Added splice to favorites aray and catch some pokemon message if favorites is empty

--- favorite.js
+++ favorite.js
@@ -1,29 +1,58 @@
-let favorites = JSON.parse(localStorage.getItem("favoritePoke"));
-let divEl = $("#stuff");
-
-
-function displayFavorites(){
-    for(i=0;i<favorites.length; i++){
-        var pTag = document.createElement("p")
-        pTag.textContent = favorites[i];
-
-        divEl.append(pTag)
-    }
-}
-
-displayFavorites()
-
+// Set favorite pokemon to local storage.
 let favoriteList = [];
 if(localStorage.getItem("favoritePoke")){
   favoriteList = JSON.parse(localStorage.getItem("favoritePoke"))
 }
+
 let favoritePokemonButton = $("#favoriteBtn")
+
 function favoritePokemon(){
   console.log($("#selectedPokemon").text())
   favoriteList.push($("#selectedPokemon").text())
   localStorage.setItem("favoritePoke", JSON.stringify(favoriteList))
+
 }
 favoritePokemonButton.click(favoritePokemon)
+
+
+
+
+
+//Display favorite pokemon on page.
+let favorites = JSON.parse(localStorage.getItem("favoritePoke"));
+let divEl = $("#stuff");
+
+
+console.log(favorites)
+
+
+function displayFavorites(){  
+    if (favorites > [0]) {
+     for(i=0;i<favorites.length; i++){
+        var pTag = document.createElement("p")
+        pTag.textContent = favorites[i];
+
+        divEl.append(pTag)
+         }
+  
+ } else {
+        var pTag = document.createElement("p")
+        pTag.textContent = "catch some pokemon"
+    
+        divEl.append(pTag)
+        }
+
+    
+}
+function favoriteListLength(){
+    if (favorites > [5]) {
+    favorites.splice(6);   
+   
+}
+}
+favoriteListLength()
+displayFavorites()
+
 
 function pokemonHomePage() {
     console.log("clicked")
