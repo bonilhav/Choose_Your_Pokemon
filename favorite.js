10d5e03e806ab430266c4c639fd7e67005ceadd0
James Odell <jpodell21@gmail.com>
2021-06-05 11:39:10 -0600

    update favorites

--- favorite.js
+++ favorite.js
@@ -1,19 +1,29 @@
-// let favorites = JSON.parse(localStorage.getItem("favoritePoke"));
-// let divEl = $("#stuff");
+let favorites = JSON.parse(localStorage.getItem("favoritePoke"));
+let divEl = $("#stuff");
 
 
-// function displayFavorites(){
-//     for(i=0;i<favorites.length; i++){
-//         var pTag = document.createElement("p")
-//         pTag.textContent = favorites[i];
+function displayFavorites(){
+    for(i=0;i<favorites.length; i++){
+        var pTag = document.createElement("p")
+        pTag.textContent = favorites[i];
 
-//         divEl.append(pTag)
-//     }
-// }
+        divEl.append(pTag)
+    }
+}
 
-// displayFavorites()
+displayFavorites()
 
-
+let favoriteList = [];
+if(localStorage.getItem("favoritePoke")){
+  favoriteList = JSON.parse(localStorage.getItem("favoritePoke"))
+}
+let favoritePokemonButton = $("#favoriteBtn")
+function favoritePokemon(){
+  console.log($("#selectedPokemon").text())
+  favoriteList.push($("#selectedPokemon").text())
+  localStorage.setItem("favoritePoke", JSON.stringify(favoriteList))
+}
+favoritePokemonButton.click(favoritePokemon)
 
 function pokemonHomePage() {
     console.log("clicked")
